f6330f274e4e1bacb72db756aa103f3fc7abc3ae
Shay Strong <shay.strong@omniearth.net>
2019-09-10 12:17:34 -0700

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 cp s3://uw-geohack/sagely/rec_clean_demo/ rec/ --recursive --include '*rec' --exclude '*' 
-aws s3 cp s3://uw-geohack/sagely/tiles/ tiles/ --recursive
+aws s3 cp s3://uw-geohack/sagely/rec_clean_demo/ rec/ --recursive --include '*rec' --exclude '*'  --no-sign-request
+aws s3 cp s3://uw-geohack/sagely/tiles/ tiles/ --recursive --no-sign-request
 
