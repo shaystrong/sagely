f663a7785bc7f44202764a4d95927e3ec5b6c06f
shay strong <shay.strong@omniearth.net>
2019-09-10 12:44:21 -0700

    updates

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 aws s3 cp s3://uw-geohack/sagely/rec_clean_demo/ rec/ --recursive --include '*rec' --exclude '*'  --no-sign-request
 aws s3 cp s3://uw-geohack/sagely/tiles/ tiles/ --recursive --no-sign-request
+aws s3 cp s3://uw-geohack/sagely/nzwani_dg_wv3_05032019_104001004A465900.mbtiles . --no-sign-request
 
